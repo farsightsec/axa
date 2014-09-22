27c687f71137da560c0132ee4105b51cc779293f
Mike Schiffman <mschiffm@fsi.io>
2014-09-22 19:52:11 +0000

    doxygen tune-up, fix misspellings

--- axa/rad_mod.h
+++ axa/rad_mod.h
@@ -19,22 +19,22 @@
 #ifndef RAD_MOD_H
 #define RAD_MOD_H
 
-/*! \file protocol.h
+/*! \file rad_mod.h
  *  \brief AXA RAD datatypes and function declarations.
  *
  *  This file contains the RAD datatypes and function declarations.
  *  Before including this file,
- *  #define RAD_MOD_PREFIX xxx
- *  to declare axa_rad_##prefix##_open() axa_rad_##prefix##_whit(),
- *  and axa_rad_##prefix##_close()
+ *  \#define RAD_MOD_PREFIX xxx
+ *  to declare axa_rad_\#\#prefix\#\#_open() axa_rad_\#\#prefix\#\#_whit(),
+ *  and axa_rad_\#\#prefix\#\#_close()
  */
 
 #include <axa/wire.h>
 
-
 /**
- *  Only hdr.len and hdr.op are valid. hdr.len is in host byte-order.
- *  body is in wire format. 
+ *  RAD protocol format
+ *  AXA protocol header and body. Only hdr.len and hdr.op are valid; hdr.len is
+ *  in host byte-order, body is in wire format. 
  */
 typedef struct axa_rad_p axa_rad_p_t;
 struct axa_rad_p {
@@ -42,15 +42,19 @@ struct axa_rad_p {
 	axa_p_body_t	body;		/**< variable size */
 };
 
+/**
+ *  RAD parameters
+ *  Linked list of axa_rad_p_t structs
+ */
 typedef struct axa_rad_parm axa_rad_parm_t;
 struct axa_rad_parm {
-	axa_rad_parm_t	*fwd;
-	axa_rad_p_t	p;
+	axa_rad_parm_t	*fwd;       /**< next item in list */
+	axa_rad_p_t	p;              /**< RAD struct */
 };
 
 
-/*
- * All of these functions can be called concurrently by two or more RAD server
+/**
+ *  All of these functions can be called concurrently by two or more RAD server
  *	threads and so must protect themselves.
  *
  * When it is opened, the module is given
@@ -68,26 +72,54 @@ struct axa_rad_parm {
  * false=fatal error with
  *	*ctxt=NULL,
  *	*out_parms=NULL,
- *	*errmsg=NULL or an error message that will be freed by the caller. */
+ *	*errmsg=NULL or an error message that will be freed by the caller. 
+ *
+ *	\param[in] ctxt
+ *	\param[in] errmsg
+ *	\param[in] out_parms
+ *	\param[in] in_parms
+ *	\param[in] uparms
+ *	\param[in] cparms
+ *
+ *	\retval true
+ *	\retval false
+ */
 typedef bool (axa_rad_open_t)(void **ctxt, char **errmsg,
 			      const axa_rad_parm_t **out_parms,
 			      const axa_rad_parm_t *in_parms,
 			      const char *uparms, const char *cparms);
 
-/*
- * Say whether to forward a packet to the RAD client.
- * -1=error, 0=no, 1=yes
+/**
+ *  RAD watch hit.
+ *  Say whether to forward a packet to the RAD client.
+ *
+ *	\param[in] ctxt
+ *	\param[in] errmsg
+ *	\param[in] whit
+ *	\param[in] whit_len
+ *	\param[in] msg
+ *	\param[in] dgp
+ *
+ *	\retval -1 error
+ *	\retval -0 no
+ *	\retval 1 yes
  */
 typedef int (axa_rad_whit_t)(void *ctxt, char **errmsg,
 			     const axa_p_whit_t *whit, size_t whit_len,
 			     const nmsg_message_t msg,
 			     const struct nmsg_ipdg *dgp);
 
+/**
+ *  RAD close
+ *
+ *	\param[in] ctxt
+ */
 typedef void (axa_rad_close_t)(void *ctxt);
 
+/** allow RAD client parameters */
+#define AXA_RAD_CPARMS_ALLOWED	"+"
 
-#define AXA_RAD_CPARMS_ALLOWED	"+"	/* allow RAD client parameters */
-
+/** RAD prefix string */
 #define RAD_PREFIX "axa_rad_"
 
 #ifdef AXA_RAD_MOD
