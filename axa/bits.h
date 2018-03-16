889eb7a2a018443a1ef4191d38b9220c91589ea5
Mike Schiffman <mschiffm@fsi.io>
2018-03-16 11:10:55 -0700

    rename AXA_CH_MAX
    ...and treat it like a true maximum value

--- axa/bits.h
+++ axa/bits.h
@@ -91,7 +91,8 @@ typedef uint64_t axa_word_t;
 typedef struct {
 	axa_word_t m[16];
 } axa_ch_mask_t;
-#define AXA_CH_MAX	255
+/** the largest SIE channel */
+#define AXA_NMSG_CH_MAX	255
 
 /**
  *  Get a numbered bit from an array of 64-bit words
