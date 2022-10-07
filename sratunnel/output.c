1bc4acfaccb115cfe64415bfa9ef81062c2cfd6e
Jeremy C. Reed <reed@fsi.io>
2022-10-07 13:22:15 -0400

    prep for 2.2.2 release and copyrights

--- sratunnel/output.c
+++ sratunnel/output.c
@@ -1,6 +1,7 @@
 /*
  * Tunnel SIE data from an SRA or RAD server.
  *
+ *  Copyright (c) 2022 DomainTools LLC
  *  Copyright (c) 2014-2018 by Farsight Security, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
